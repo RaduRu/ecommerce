7b6e4d6caca670d0129aad023cad7c6c7ab6f27c
RaduRu <radu.ursu@stud.unifi.it>
2024-06-14 13:29:49 +0200

    first commit

---
+++ store/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Product
+
+
+# Create your views here.
+def home(request):
+    products = Product.objects.all()
+    return render(request, "home.html", {'products':products})
+
+
+def about(request):
+    return render(request,"about.html", {})
+
